20bb150e0bf0be326464d7c45ab4f2b2fddfa5c1
Lenard Gunda <lenard@offbeat.fi>
2015-06-11 15:53:45 +0300

    Fixed: migration version id is read as a long from command line

---
+++ src/FluentMigrator.Runner.Aspnet/DocoptExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DocoptNet;
+
+namespace FluentMigrator.Runner.Aspnet
+{
+    public static class DocoptExtensions
+    {
+		public static long AsLong ( this ValueObject obj ) {
+			if (!obj.IsList)
+				return Convert.ToInt64(obj.Value);
+			return 0;
+		}
+	}
+}
