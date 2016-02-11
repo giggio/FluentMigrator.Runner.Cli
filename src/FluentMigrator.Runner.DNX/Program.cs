165ee041d0081300f62393eb9d49e202c6d52d34
Giovanni Bassi <giggio@giggio.net>
2016-02-11 18:43:10 -0200

    Support noConnection and startVersion params
    Update to beta2

--- src/FluentMigrator.Runner.DNX/Program.cs
+++ src/FluentMigrator.Runner.DNX/Program.cs
@@ -14,7 +14,7 @@ namespace FluentMigrator.Runner.DNX
 {
     public class Program
     {
-        private IApplicationEnvironment appEnvironment;
+        private readonly IApplicationEnvironment appEnvironment;
 
         public Program(IApplicationEnvironment appEnvironment)
         {
@@ -25,9 +25,10 @@ namespace FluentMigrator.Runner.DNX
         {
             const string usage = @"Fluent Migrator DNX Runner
   Usage:
-    dnx . run --provider PROVIDER --connectionString CONNECTION [--assembly ASSEMBLY] [--output FILE] [--task TASK] [--migrateToVersion VERSION] [--profile PROFILE] [--tag TAG] [--verbose]
-    dnx . run --version
-    dnx . run --help
+    dnx run --provider PROVIDER --connectionString CONNECTION [--output FILE] [--assembly ASSEMBLY] [--task TASK] [--migrateToVersion END] [--profile PROFILE] [--tag TAG] [--verbose]
+    dnx run --provider PROVIDER --noConnection --output FILE [--assembly ASSEMBLY] [--task TASK] [--startVersion START] [--migrateToVersion END] [--profile PROFILE] [--tag TAG] [--verbose]
+    dnx run --version
+    dnx run --help
 
   Options:
     --provider PROVIDER -p PROVIDER                 Database type. Possible values:
@@ -48,7 +49,9 @@ namespace FluentMigrator.Runner.DNX
                                                     It will default to the current path.
     --output FILE -o FILE                           File to output the script. If specified will write to a file instead of running the migration. [default: migration.sql]
     --task TASK -t TASK                             The task to run. [default: migrate]
-    --migrateToVersion VERSION                      The specific version to migrate. Default is 0, which will run all migrations. [default: 0]
+    --noConnection                                  Indicates that migrations will be generated without consulting a target database. Should only be used when generating an output file.
+    --startVersion START                            The specific version to start migrating from. Only used when NoConnection is true. [default: 0]
+    --migrateToVersion END                          The specific version to migrate. Default is 0, which will run all migrations. [default: 0]
     --profile PROFILE                               The profile to run after executing migrations.
     --tag TAG                                       Filters the migrations to be run by tag.
     --verbose                                       Verbose. Optional.
@@ -56,11 +59,15 @@ namespace FluentMigrator.Runner.DNX
     --version -v                                    Show version.
 ";
 
-            var argsWithRun = new[] { ".", "run" }.Union(args).ToArray();
+            var argsWithRun = new[] { "run" }.Union(args).ToArray();
             var arguments = new Docopt().Apply(usage, argsWithRun, version: Assembly.GetExecutingAssembly().GetName().Version, exit: true);
             verbose = arguments["--verbose"].IsTrue;
             provider = arguments["--provider"].ToString();
-            connection = arguments["--connectionString"].ToString();
+            noConnection = arguments["--noConnection"].IsTrue;
+            if (noConnection)
+                startVersion = arguments["--startVersion"].AsLong();
+            else
+                connection = arguments["--connectionString"].ToString();
             assembly = (arguments["--assembly"] != null)
                 ? assembly = arguments["--assembly"].ToString()
                 : appEnvironment.ApplicationBasePath;
@@ -112,13 +119,17 @@ namespace FluentMigrator.Runner.DNX
             if (verbose) WriteLine($"Found dnu: {dnuCmd}");
             var cmd = Environment.GetEnvironmentVariable("ComSpec");
             if (verbose) WriteLine($"Building project directory: {assembly}");
-            var processStartInfo = new ProcessStartInfo(cmd, $@"/c ""{dnuCmd}"" build --quiet --configuration Debug --framework dnx451");
-            processStartInfo.WorkingDirectory = assembly;
-            processStartInfo.UseShellExecute = false;
+            var processStartInfo = new ProcessStartInfo(cmd, $@"/c ""{dnuCmd}"" build --quiet --configuration Debug --framework dnx451")
+            {
+                WorkingDirectory = assembly,
+                UseShellExecute = false
+            };
             if (!verbose) processStartInfo.RedirectStandardOutput = true;
-            var process = new Process { StartInfo = processStartInfo };
-            process.Start();
-            process.WaitForExit();
+            using (var process = new Process { StartInfo = processStartInfo })
+            {
+                process.Start();
+                process.WaitForExit();
+            }
             var migrationsDllPath = Path.Combine(assembly, @"bin\Debug\dnx451", Path.GetFileName(assembly) + ".dll");
             if (!File.Exists(migrationsDllPath))
             {
@@ -130,9 +141,11 @@ namespace FluentMigrator.Runner.DNX
 
         private readonly ConsoleAnnouncer consoleAnnouncer = new ConsoleAnnouncer();
         private bool verbose;
+        private bool noConnection;
         private string provider;
         private string connection;
         private string task;
+        private long startVersion;
         private long migrateToVersion;
         private string profile;
         private IEnumerable<string> tags;
@@ -172,6 +185,8 @@ namespace FluentMigrator.Runner.DNX
                 Targets = new[] { assembly },
                 PreviewOnly = false,
                 Task = task,
+                NoConnection = noConnection,
+                StartVersion = startVersion,
                 Version = migrateToVersion,
                 Profile = profile,
                 Tags = tags
